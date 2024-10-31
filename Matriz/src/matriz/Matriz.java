978f68877f5315a1f5662117885d5630f3ed472f
Yoquer_13 <roicastrocalvar@gmail.com>
2024-10-31 18:03:24 +0100

    Algo

--- Matriz/src/matriz/Matriz.java
+++ Matriz/src/matriz/Matriz.java
@@ -15,6 +15,16 @@ public class Matriz {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int matrizf = 5;
+        int matrizc = 3;
+        int matrizi = 10;
+        for (int e = 0; e < matrizc;e++) {
+            matrizi = matrizi - e;
+            System.out.println();
+        for (int i = 0; i < matrizf;i++) {
+            System.out.print((matrizi-i)+"  ");
+        }
     }
-    
+    }
+
 }
