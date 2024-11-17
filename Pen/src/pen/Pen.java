c10b9bab84f6ac7d6dfb07c987b104ff9bb0e74e
Yoquer_13 <roicastrocalvar@gmail.com>
2024-11-17 14:02:54 +0100

    Pen
    Comentarios

--- Pen/src/pen/Pen.java
+++ Pen/src/pen/Pen.java
@@ -10,55 +10,68 @@ package pen;
  */
 public class Pen {
 
+    
     private int ink = 30;
 
     /**
-     * Cantida de tinta que tiene el objeto.
+     * Obtiene la cantidad actual de tinta que tiene el bolígrafo.
      *
-     * @return
+     * @return La cantidad de tinta disponible.
      */
     public int getInk() {
         return ink;
     }
 
     /**
+     * Permite establecer una nueva cantidad de tinta para el bolígrafo.
      *
-     * @param ink modificacion a la cantidad de tinta, por defecto 30.
+     * @param ink Nueva cantidad de tinta a establecer. Por defecto es 30.
      */
     public void setInk(int ink) {
         this.ink = ink;
     }
 
     /**
-     * Objeto de tipo Pen .
+     * Constructor por defecto de la clase `Pen`. Inicializa el bolígrafo con la
+     * cantidad de tinta predeterminada.
      */
     public Pen() {
     }
 
     /**
-     * @param args the command line arguments
+     * Método principal del programa. Aquí se crean objetos de tipo `Pen` y se
+     * utilizan para escribir texto.
+     *
+     * @param args Argumentos de la línea de comandos (no utilizados en este
+     * programa).
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+   
         Pen bic = new Pen();
+
+  
         bic.write("Arbol Cielo Mar Rapido Suenho Luz Rio Montanha Tiempo Sol Estrella Lluvia Paz Enfado Fuego Chispas Ceniza Camino Amor Lealtad");
         bic.write("Soy mauricio tengo tres anhos y soy de murcia y te recuerdo que murcia existe");
         bic.write("Me llamo mauricio tengo tres anhos");
-
     }
 
+    /**
+     * Método que permite escribir texto utilizando el bolígrafo. Cada palabra
+     * escrita consume una unidad de tinta. Si no hay suficiente tinta, no se
+     * puede escribir el texto.
+     *
+     * @param text El texto que se desea escribir.
+     */
     public void write(String text) {
 
         int cantidadEspacios = 0;
 
-        //Cuenta la cantidad de espacios
         for (int i = 0; i < text.length(); i++) {
             if (text.charAt(i) == ' ') {
                 cantidadEspacios++;
             }
         }
 
-        //Si hai una palabra que la cuente
         if (text.length() != 0) {
             cantidadEspacios++;
         }
@@ -67,8 +80,8 @@ public class Pen {
             ink -= cantidadEspacios;
             System.out.println(text);
         } else {
+
             System.out.println("Non queda a suficiente tinta.");
         }
-
     }
 }
