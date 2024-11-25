08ffc3603ab56071108e2921ff32f2b8a32d60ce
Yoquer_13 <roicastrocalvar@gmail.com>
2024-11-25 09:56:58 +0100

    DataBase

--- DataBase/src/database/DataBase.java
+++ DataBase/src/database/DataBase.java
@@ -5,6 +5,7 @@
 package database;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -12,25 +13,62 @@ import java.util.ArrayList;
  */
 public class DataBase {
 
+    private ArrayList<Client> dataBase;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        DataBase paco = new DataBase();
+        Scanner scan = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("Menu");
+            System.out.println("Opciones");
+            System.out.println("1. Añade un cliente");
+            System.out.println("2. Enseña todos los clientes");
+            System.out.println("3. Elimina un cliente");
+            System.out.println("4. Enseña la cantidad de clientes");
+            System.out.println("5. Salir");
+            System.out.println("Elije una opcion");
+
+            option = scan.nextInt();
+        } while (option != 5);
+
     }
-    public void addClient(){
-        ArrayList grupo = new ArrayList(); 
-         
+
+    public void addClient() {
+
+        dataBase = new ArrayList();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe el nif del cliente");
+        String nif = scanner.nextLine();
+        System.out.println("Escribe el nombre del cliente");
+        String name = scanner.nextLine();
+        System.out.println("Escribe el apellido del cliente");
+        String surname = scanner.nextLine();
+        Client Client = new Client(nif, name, surname);
+        dataBase.add(Client);
     }
-    public void showClients(){
-        
+
+    public void showClients() {
+        for (int i = 0; i < dataBase.size(); i++) {
+            ;
+            System.out.println(dataBase.get(i).getNif());
+            
+        }
+
     }
-    public void removeClient(String nif){
-        
+
+    public void removeClient(String nif) {
+
     }
-    public int numClients(){
-        
+
+    public int numClients() {
+
         return 0;
-        
+
     }
 }
