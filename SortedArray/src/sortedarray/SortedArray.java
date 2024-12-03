b993ecf0c0eef8db447d985aee233ad0da2bf1fe
Yoquer_13 <roicastrocalvar@gmail.com>
2024-12-03 11:15:33 +0100

    SortedArray

--- SortedArray/src/sortedarray/SortedArray.java
+++ SortedArray/src/sortedarray/SortedArray.java
@@ -5,34 +5,83 @@
 package sortedarray;
 
 /**
- *
+ * Clase que implementa un arreglo de enteros y proporciona métodos para
+ * ordenarlo y mostrar su contenido.
+ * 
  * @author roi.castrocalvar
  */
 public class SortedArray {
 
-    private int[] array = new int[]{14, 165, 2, 18, 19, 14};
+    /**
+     * Arreglo de enteros que se manipulará en la clase.
+     */
+    private int[] array = new int[]{14, 165, 2, 18, 19, 16};
 
     /**
-     * @param args the command line arguments
+     * Obtiene el arreglo de enteros actual.
+     *
+     * @return El arreglo de enteros.
+     */
+    public int[] getArray() {
+        return array;
+    }
+
+    /**
+     * Establece un nuevo arreglo de enteros.
+     *
+     * @param array El nuevo arreglo a establecer.
+     */
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+
+    /**
+     * Método principal que ejecuta el programa. Crea una instancia de 
+     * {@code SortedArray}, ordena el arreglo y muestra su contenido.
+     *
+     * @param args Argumentos de la línea de comandos (no se utilizan).
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        SortedArray array = new SortedArray();
-        array.sort();
-        array.show();
+        // Crear una instancia de SortedArray
+        SortedArray myArray = new SortedArray();
+        myArray.sort();
+        myArray.show();
     }
 
-    public void sort() {
-        for (int i = 0; i < array.length; i++) {
-            if(array[i]>(array[i]+1)){
-                System.out.print("perro  ");
+    /**
+     * Método que ordena el arreglo en orden ascendente utilizando el
+     * algoritmo de ordenamiento de burbuja.
+     *
+     * @return El arreglo ordenado.
+     */
+    public int[] sort() {
+        // Variable para controlar si hubo intercambio en la pasada actual
+        boolean swapped = true;
+        // Bucle que recorre el arreglo desde el final hasta el principio
+        for (int i = array.length - 1; i > 0 && swapped; i--) {
+            swapped = false; // Reiniciar el estado de intercambio
+            
+            // Bucle interno para comparar y ordenar elementos adyacentes
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) { 
+                    // Intercambiar los elementos si están fuera de orden
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true; // Indicar que hubo intercambio
+                }
             }
         }
+        return array; 
     }
 
+    /**
+     * Método que muestra los elementos del arreglo en una sola línea, 
+     * separados por espacios.
+     */
     public void show() {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+        for (int myArray:array){
+            System.out.print(myArray + " ");
         }
     }
 }
