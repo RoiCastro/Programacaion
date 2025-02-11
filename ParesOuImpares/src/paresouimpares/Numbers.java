b38c95c2a6f87bf92116b9545c2147f3eec43f94
Dem0nGrFt <157622302+Dem0nGrFt@users.noreply.github.com>
2025-02-11 12:48:16 +0100

    .

---
+++ ParesOuImpares/src/paresouimpares/Numbers.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paresouimpares;
+
+/**
+ *
+ * @author roi.castrocalvar
+ */
+public class Numbers {
+    private int min = 0;
+    private int max = 100;
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+    
+}
